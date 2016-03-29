dc526b4e970d9a2fd0f6ed646ccbc09a612c1578
beth1118 <beth1118@naver.com>
2016-03-29 18:02:21 +0900

    과제 201611093
    asdaxcvzz

---
+++ W4Main.py.py
@@ -0,0 +1,13 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.home()
+t1.clear()
+def makeswirlsSquare(size,bigger,sides,angle):
+    for i in range(0,sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+makeswirlsSquare(10,10,30,90)
+wn.exitonclick()
