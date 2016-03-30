83d8fad2b69a72a80f2516b2af28565f2abc3b22
beth1118 <beth1118@naver.com>
2016-03-30 11:58:37 +0900

    Update W4Main.py.py

--- W4Main.py.py
+++ W4Main.py.py
@@ -11,3 +11,19 @@ def makeswirlsSquare(size,bigger,sides,angle):
             t1.right(angle)
 makeswirlsSquare(10,10,30,90)
 wn.exitonclick()
+
+#바람개비 모형 그리기 다이어그램  
+%%plantuml  
+ @startuml 
+  
+ start  
+ :set size, bigger, sides, angle;  
+ repeat  
+ if (i is multiples of 2) then  
+    :size=existing size+bigger;  
+ endif  
+ :t1.fd(size);  
+ :t1.right(angle);  
+ repeat while(sides)  
+ stop  
+ @enduml  
