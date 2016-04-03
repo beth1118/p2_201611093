ea06cf76a56120b8e1255e73cf816a93d5f307fa
beth1118 <beth1118@naver.com>
2016-04-03 18:46:32 +0900

    Added files via upload
    oo

---
+++ W5_homework2.py.py
@@ -0,0 +1,31 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def main():  
+    print ("Let's play R S P game!!") 
+    p1=input("hey first guy what's your choice?: ")
+    p2=input("how about you,next guy: ") 
+def subtool(p):  
+    if(p=='r'):  
+        return 2.0  
+    elif(p=='s'):  
+        return 4.0  
+    elif(p=='p'):  
+        return 8.0  
+    else:  
+        print ("Input Error")          
+    alpha=subtool(p1)/subtool(p2)  
+    if(alpha==0.5):  
+        print ("p1 you win!!")  
+    elif(alpha==1):  
+        print ("draw :)")  
+    elif(alpha==0.25):  
+        print ("p2 you win!!")  
+    elif(alpha==2):  
+        print ("p2 you win!!")  
+    elif(alpha==4):  
+        print ("p1 you win!!")  
+        
+main()
+ 
