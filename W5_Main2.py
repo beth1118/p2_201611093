a8e5789de1fa095d4552ae4e6031781daf755037
beth1118 <beth1118@naver.com>
2016-04-04 22:57:44 +0900

    Added files via upload
    rr

---
+++ W5_Main2.py
@@ -0,0 +1,17 @@
+﻿def BMI():
+    height=raw_input("input user height(m):")
+    weight=raw_input("input user weight(kg):")
+    BMI=weight/(height*height)
+       print BMI
+    if BMI <= 18.5:
+        res="low weight"
+    elif 18.5<BMI<25:
+        res="normal weight"
+    elif 25<=BMI<30:
+        res="overweight"
+    elif 30<=BMI:
+        res="obesity"
+    return res
+
+print BMI()
+        
