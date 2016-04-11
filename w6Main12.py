8bc74b5f0877dcd11dd4140cecb613202a0a5902
beth1118 <beth1118@naver.com>
2016-04-11 17:01:15 +0900

    나누기 4=0 이고 5=/0 일때 1~1000까지 더하기

---
+++ w6Main12.py
@@ -0,0 +1,27 @@
+﻿x=list()
+
+def sumList(aList):
+    sum=0
+    for i in range(0,1001):
+        if(i%4==0 and i%5>0):
+            x.append(i)
+    for t in range(0,len(x)):
+        sum=sum+x[t]    
+    return sum
+
+def lab6():
+    """programming is really hahaha fun"""
+    aList=[x]
+    labsum=sumList(aList)
+    print labsum
+
+def main():
+    lab6()
+
+def main():
+	lab6()
+
+if __name__=="__main__":
+	main()
+
+wn=raw_input("Do you want finish? Input yes and press Enter  ")
