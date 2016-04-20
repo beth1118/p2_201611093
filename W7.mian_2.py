3f8c83d1f293bc274dbc32df10a5ab629b8d40cc
beth1118 <beth1118@naver.com>
2016-04-20 13:52:06 +0900

    Added files via upload
    rrr

---
+++ W7.mian_2.py
@@ -0,0 +1,72 @@
+def saveTracks(): 
+    import turtle
+    wn=turtle.Screen()
+
+    t1=turtle.Turtle()
+    t1.speed(1)
+    t1.penup()
+
+    mytracks=list()
+    t1.goto(-400,300)
+
+    mytracks.append(t1.pos())
+
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(400)
+
+    mytracks.append(t1.pos())
+
+    t1.backward(150)
+    t1.left(90)
+
+    mytracks.append(t1.pos())
+
+    t1.fd(300)
+    t1.left(90)
+
+    mytracks.append(t1.pos())
+
+    t1.fd(300)
+
+    mytracks.append(t1.pos())
+
+    t1.back(150)
+    t1.right(90)
+
+    mytracks.append(t1.pos())
+
+    t1.fd(300)
+    t1.left(90)
+
+    mytracks.append(t1.pos())
+
+    t1.right(90)
+    t1.fd(200)
+
+    mytracks.append(t1.pos())
+
+    t1.fd(300)
+    t1.back(100)
+
+    mytracks.append(t1.pos())
+
+    t1.left(90)
+    t1.fd(200)
+    mytracks.append(t1.pos())
+    return mytracks
+
+def replayTracks(mytracks):
+    for t in mytracks:
+        print t
+
+
+def lab7():
+    mytracks=saveTracks()
+    replayTracks(mytracks)
+    
+def main():
+    lab7()
+if __name__=="__main__":
+    main()
